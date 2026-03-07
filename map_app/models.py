6e6b4575c2beb4c4e10674aae1e61898d1e30402
sarifishh <sarifishh@yahoo.com>
2026-03-07 17:41:54 +0000

    FIX model name and attrs

--- map_app/models.py
+++ map_app/models.py
@@ -1,7 +1,27 @@
 from django.db import models
 
 # Create your models here.
-class StudyLocations(models.Model):
+class Locations(models.Model):
+    TYPE_CHOICES = [
+    ('study', 'Study Space'), 
+    ('sweet_treat', 'Sweet Treat')
+]
     name = models.CharField(max_length=120)
     latitude = models.FloatField()
     longitude = models.FloatField()
+    address = models.CharField(max_length=255, blank=True, null=True)
+    
+    # This field tells the map which icon to show
+    location_type = models.CharField(
+        max_length=20, 
+        choices=TYPE_CHOICES, 
+        default='study'
+    )
+
+    # for the treats to not rewrite the same cafe 
+    google_place_id = models.CharField(max_length=255, unique=True, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.location_type})"
+
+
