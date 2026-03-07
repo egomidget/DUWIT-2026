8fe0a699870c5e9e9c381df0433c2d554adc35e9
egomidget <moxonmercedes@hotmail.com>
2026-03-07 20:43:48 +0000

    CHANGE class to className

--- frontend/src/components/StudySpace.jsx
+++ frontend/src/components/StudySpace.jsx
@@ -3,35 +3,35 @@ import AttrBox from "./AttrBox";
 import CandyDivider from "./CandyDivider";
 export default function StudySpace({ name, location, sugar_level, image_url, description , rating, locationLink}) {
     return (
-    <div class="container mt-5">
-        <div class="card study-card-rush shadow-lg">
+    <div className="container mt-5">
+        <div className="card study-card-rush shadow-lg">
             <Badge>Open Now</Badge>
 
-            <div class="card-body p-4">
-                <div class="d-flex justify-content-between align-items-start mb-3">
+            <div className="card-body p-4">
+                <div className="d-flex justify-content-between align-items-start mb-3">
                     <div>
-                        <h2 class="sugar-font text-pink mb-0">{name}</h2>
-                        <p class="text-muted"><i class="bi bi-geo-alt"></i>{location}</p>
+                        <h2 className="sugar-font text-pink mb-0">{name}</h2>
+                        <p className="text-muted"><i className="bi bi-geo-alt"></i>{location}</p>
                     </div>
-                    <div class="rating-bubble">
-                        <span class="sugar-font">{rating}</span>
+                    <div className="rating-bubble">
+                        <span className="sugar-font">{rating}</span>
                     </div>
                 </div>
 
                 <CandyDivider/>
 
-                <div class="row g-3 text-center mb-4">
+                <div className="row g-3 text-center mb-4">
                     <AttrBox title="Sugar Levels" value="Quiet" />
                     <AttrBox title="Wifi" value="900 Mbps" />
                     <AttrBox title="Outlets" value="Plenty" />
                 </div>
 
-                <h5 class="sugar-font small-head">The Lowdown</h5>
-                <p class="card-text">{description}</p>
+                <h5 className="sugar-font small-head">The Lowdown</h5>
+                <p className="card-text">{description}</p>
 
-                <div class="d-grid gap-2 mt-4">
-                    <a class="btn btn-go-time" href={locationLink}>BOOK MY SPOT</a>
-                    {/* <button class="btn btn-outline-sugar">View Menu</button> */}
+                <div className="d-grid gap-2 mt-4">
+                    <a className="btn btn-go-time" href={locationLink}>BOOK MY SPOT</a>
+                    {/* <button className="btn btn-outline-sugar">View Menu</button> */}
                 </div>
             </div>
         </div>
