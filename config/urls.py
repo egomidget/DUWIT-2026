9f0b0ac85ad4aeab9133867c4516a8781b6b0d22
Prema Poudyal <premapoudyalx@gmail>
2026-03-07 17:15:56 +0000

    ADD models for study spaces and facilities

--- config/urls.py
+++ config/urls.py
@@ -1,29 +1,7 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from projectApp.views import study_spaces_api_dumby, study_sapce_api_dumby
-
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls), 
-    path('', include("projectApp.urls")),  
-    path('api/dumby-endpoint/', study_spaces_api_dumby, name="dumby"),
-    path('api/dumby-endpoint-space/1/',study_sapce_api_dumby, name="dumby-sapce"),
-
-    path('api/map/', include('map_app.urls')),
+    path('admin/', admin.site.urls),
+    path('', include("projectApp.urls")),
 ]
--- projectApp/models.py
+++ projectApp/models.py
@@ -1,3 +1,89 @@
 from django.db import models
 
-# Create your models here.
+class Studyspaces(models.Model): 
+
+    temp_choices = [
+        ("Artic", "Artic"), 
+        ("Intense aircon", "Intense aircon"), 
+        ("Just right", "Just right"),
+        ("Nice and toasty", "Nice and toasty"),   
+        ("Sahara Desert", "Sahara Desert"), 
+    ]
+
+    window_choices = [
+        ("Bat cave", "Bat cave"), 
+        ("Airplane window", "Airplane window"), 
+        ("Have to go on a bear hunt to find", "Have to go on a bear hunt to find"),
+        ("Green house", "Green house"),
+    ]
+
+    sound_choices = [
+        ("Pin drop", "Pin drop"),
+        ("Social space", "Social space"),
+        ("Mothers meeting", "Mothers meeting"),
+        
+    ]
+
+    ambience_choices = [
+        ("Dark academia", "Dark academia"),
+        ("warm and cosy", "warm and cosy"),
+        ("corporate", "Corporate "),
+        ("Millennium core", "Millennium core"),
+        ("Bare bones", "Bare bones"),
+        ("Brutalistic", "Brutslistic"),
+    ]
+
+    rating_choices = [
+    (1, "1"),
+    (2, "2"),
+    (3, "3"),
+    (4, "4"),
+    (5, "5"),
+]
+
+    
+    name = models.CharField(max_length=100)
+    indoors = models.BooleanField(default = False)
+    wifi = models.BooleanField(default = False)
+    temperature = models.CharField(
+        max_length = 1000,
+        choices = temp_choices
+)
+    plugsockets = models.BooleanField(default = False)
+    windows = models.CharField(
+        max_length = 1000,
+        choices = window_choices 
+)
+    sound = models.CharField(
+        max_length = 1000, 
+        choices = sound_choices
+)
+    description = models.CharField(max_length= 1000) 
+    ambience = models.CharField(
+    max_length=1000,
+    choices=ambience_choices
+)
+    
+    in_house_food = models.BooleanField(default = False)
+    pay_to_enter = models.BooleanField(default = False)
+    public = models.BooleanField(default = False)
+    accessibility = models.BooleanField(default = False)
+    opening_time = models.TimeField(null=True, blank=True)
+    closing_time = models.TimeField(null=True, blank=True)
+    rating = models.IntegerField(
+        choices=rating_choices,
+        null=True, blank=True)
+    
+class facilities(models.Model): 
+    venting_machine = models.BooleanField(default = False)
+    microwave = models.BooleanField(default = False)
+    water = models.BooleanField(default = False)
+    aircon = models.BooleanField(default = False)
+    toilets = models.BooleanField(default = False)
+    hottap = models.BooleanField(default = False)
+    meetingroom = models.BooleanField(default = False)
+    printer = models.BooleanField(default = False)
+    pcs = models.BooleanField(default = False)
+
+    def __str__(self):
+        return self.name
