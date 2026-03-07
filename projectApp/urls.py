d7a695774c250e699b0ae0286fe4541a9d9ede98
egomidget <moxonmercedes@hotmail.com>
2026-03-07 22:01:36 +0000

    ADD todo urls for someone to do

--- projectApp/urls.py
+++ projectApp/urls.py
@@ -4,4 +4,6 @@ from .views import studyspaces_list, space_options
 urlpatterns = [
     path("", studyspaces_list),
     path("options/", space_options),
+    # TODO pls
+    # path("add/") i need a post endpoint for the study spaces, use serializer again
 ]
