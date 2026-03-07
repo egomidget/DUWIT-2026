05bc049bee59fd1bca3245d56d62aa427bec8473
egomidget <moxonmercedes@hotmail.com>
2026-03-07 19:21:52 +0000

    FIX url error

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from projectApp.views import study_spaces_api_dumby, study_sapce_api_dumby
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("projectApp.urls")),
+    path('api/dumby-endpoint/', study_spaces_api_dumby, name="dumby"),
+    path('api/dumby-endpoint-space/1/',study_sapce_api_dumby, name="dumby-sapce"),
+
+    path('api/map/', include('map_app.urls')),
 ]
