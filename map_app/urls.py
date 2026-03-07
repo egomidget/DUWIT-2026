ff3ce5497fdf83765d603abe570e3042072d0094
sarifishh <sarifishh@yahoo.com>
2026-03-07 19:27:06 +0000

    ADD parameters to sweet treat api

--- map_app/urls.py
+++ map_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/treats/', views.get_sweet_treats, name='get_treats'),
+    path('treats/', views.get_sweet_treats, name='get_treats'),
 ]
--- map_app/views.py
+++ map_app/views.py
@@ -7,14 +7,12 @@ from django.conf import settings
 # Create your views here.
 def get_sweet_treats(request):
     api_key = os.getenv('GOOGLE_MAPS_API_KEY')
-    print(f"DEBUG: My key is {api_key}") # This will show up in your TERMINAL, not the browser
-
-    #centering in durham for now
-    lat = "54.768115286762736" 
-    lng = "-1.5733532842088882"
-    
-    #2km from durham center search radius for now
-    radius = 2000 
+   
+    #getting the parameters tho with a default set to billy b
+    #this is how itll be fetched fetch(`http://127.0.0.1:8000/api/map/treats/?lat=${insert lat variable here}&lng=${insert longitude variable here}&radius=1500`)
+    lat = request.GET.get('lat', '54.7651')
+    lng = request.GET.get('lng', '-1.5772')
+    radius = request.GET.get('radius', '1500') # Default 1.5km
 
     #request to google
     keywords = "bakery|dessert|ice cream|gelato|sweets|pastry|cake|cookies|confectionary|Pâtisserie|Patisserie|sorbet|frozen yoghurt"
