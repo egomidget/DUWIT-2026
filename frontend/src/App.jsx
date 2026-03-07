9a16714cb9a7d471ef3c6f66841156891c9f2d84
egomidget <moxonmercedes@hotmail.com>
2026-03-07 17:16:13 +0000

    ADD study spaces directory on homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,25 @@
-import Card from "./components/Card"
+import Card from "./components/Card";
+import StudySpaces from "./components/StudySpaces";
+import CandyDivider from "./components/CandyDivider";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './App.css';
 
 function App() {
   return (
   <>
-    <Card header='This is a header'>Woah this is the body</Card>
-    <Card>Woah this is the body</Card>
+    <div className="container-fluid py-5">
+      <div className="row justify-content-center align-items-center">
+        <div className="col-md-6">
+          <Card header='Study Space Directory'>Welcome to the root directory fo the Study Spaces. Feel free to explore them below!</Card>
+        </div>
+      </div>
+
+      <CandyDivider />
+
+      <StudySpaces />
+    </div>
   </>
   );
 }
