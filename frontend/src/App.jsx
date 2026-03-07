812157a8b166ec9acdfcf86341379b2dbfb273dd
egomidget <moxonmercedes@hotmail.com>
2026-03-07 14:25:22 +0000

    ADD app component for testing

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import Card from "./components/Card"
+
+import './App.css';
+
+function App() {
+  return (
+  <>
+    <Card header='This is a header'>Woah this is the body</Card>
+    <Card>Woah this is the body</Card>
+  </>
+  );
+}
+
+export default App;
