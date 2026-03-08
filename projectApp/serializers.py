e02e7e7e4b5ef35457a0d3739f12b69f3173e74f
egomidget <moxonmercedes@hotmail.com>
2026-03-08 04:13:36 +0000

    FIX create study spaces endpoints

--- projectApp/serializers.py
+++ projectApp/serializers.py
@@ -6,7 +6,13 @@ class SpacesSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Studyspaces
-        fields = "__all__"
+        fields = '__all__'
+        extra_kwargs = {
+            'ambience': {'required': False, 'allow_null': True},
+            'temperature': {'required': False, 'allow_null': True},
+            'windows': {'required': False, 'allow_null': True},
+            'sound': {'required': False, 'allow_null': True},
+        }
 
     def get_opening_hours(self, obj):
         return obj.get_opening_hours()
--- projectApp/urls.py
+++ projectApp/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("", studyspaces_list),
     path("options/", space_options),
     path("<int:id>/", studyspace_detail),
-    path("studyspaces/create/", Studypace_create),
+    path("create/", Studypace_create),
 
     # TODO pls
     # path("add/") i need a post endpoint for the study spaces, use serializer again
