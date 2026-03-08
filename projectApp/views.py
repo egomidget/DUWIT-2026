719a4bc4f3ca95a589ef522f0e2696878edd3b1b
Prema Poudyal <premapoudyalx@gmail>
2026-03-08 01:53:17 +0000

    ADD post for study space

--- projectApp/views.py
+++ projectApp/views.py
@@ -6,7 +6,6 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
-from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 
 from .models import Studyspaces
